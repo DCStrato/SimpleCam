86c4674be702b9caca675b8283ff18652ec054ab
DCStrato <william@zaggle.com>
2021-11-01 21:51:56 -0600

    Added Preset Pages  Fixed a few small bugs

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -1,4 +1,21 @@
-﻿Imports System.Configuration
+﻿'Copyright 2020 William Zaggle (DC STRATO)
+
+'Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+'and associated documentation files (the "Software"), to deal in the Software without restriction,
+'including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+'and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+'subject to the following conditions:
+
+'The above copyright notice and this permission notice shall be included in all copies or substantial
+'portions of the Software.
+
+'THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+'LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+'IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+'WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+'OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Imports System.Configuration
 Imports System.Web.Services
 Imports System.Web.Services.Protocols
 Imports System.ComponentModel
@@ -262,7 +279,7 @@ Class MainWindow
                 ReDim myReadBuffer(1)
                 Return myReadBuffer
             End Try
-            If IsCMD And numberOfBytesRead < 4 Then 'Receive Completion
+            If IsCMD And numberOfBytesRead <4 Then 'Receive Completion
                 Try
                     numberOfBytesRead = serverStream1.Read(myReadBuffer, 0, myReadBuffer.Length)
                 Catch ex As System.IO.IOException
@@ -278,8 +295,8 @@ Class MainWindow
             clientSocket.Close()
             ReDim Preserve myReadBuffer(numberOfBytesRead - 1)
             Return myReadBuffer
-        Else 'Do Serial
-            settings.Content = "Com Port2"
+        Else ' Do Serial
+            settings.Content="Com Port2"
             Dim ComPort As New SerialPort
             If OpenComPort(My.Settings.CamCom(Cam - 1), ComPort) Then
                 ComPort.Write(Command, 0, Command.Length)
